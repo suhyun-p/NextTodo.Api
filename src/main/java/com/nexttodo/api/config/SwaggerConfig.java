44469634fa5a4e4bfae82c3641348870bf5a61e1
suhpark <laztwo@hanmail.net>
2021-10-16 14:03:57 +0900

    add Swagger

---
+++ src/main/java/com/nexttodo/api/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.nexttodo.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("NextTodo.Api")
+                .description("nextTodo.api")
+                .build();
+    }
+
+    @Bean
+    public Docket commonApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("default")
+                .apiInfo(this.apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.nexttodo.api.controller"))
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+}

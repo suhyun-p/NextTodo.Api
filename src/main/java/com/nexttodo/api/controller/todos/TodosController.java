8ca20e2c011929ffcd0d2d106ce6f9cc33cb0e1f
suhpark <laztwo@hanmail.net>
2021-10-17 22:36:46 +0900

    수

--- src/main/java/com/nexttodo/api/controller/todos/TodosController.java
+++ src/main/java/com/nexttodo/api/controller/todos/TodosController.java
@@ -19,7 +19,16 @@ public class TodosController {
 
     @GetMapping("/todos")
     public List<TodoMain> todos() {
-        return todosService.todos();
+        List<TodoMain> todos = todosService.todos();
+        if(todos.isEmpty()) {
+            todos.add(TodoMain.builder().id((long)1).text("마트 가서 장보기").color("red").checked(false).build());
+            todos.add(TodoMain.builder().id((long)2).text("수학 숙제하기").color("orange").checked(false).build());
+            todos.add(TodoMain.builder().id((long)3).text("코딩하기").color("yellow").checked(true).build());
+            todos.add(TodoMain.builder().id((long)4).text("넥스트 공부하기").color("green").checked(true).build());
+            todos.add(TodoMain.builder().id((long)5).text("요리 연습하기").color("blue").checked(false).build());
+            todos.add(TodoMain.builder().id((long)6).text("분리수거 하기").color("navy").checked(false).build());
+        }
+        return todos;
     }
 
     @PostMapping("/todos")
